db05355234e59fa2b1b2ecc774f60e6eef205784
Shakim Ahamed <sakimahamed7@gmail.com>
2024-09-25 12:13:22 +0300

    3rd Commit bug fixing app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import { ChangeEvent, FC, useEffect, useState } from 'react';
 import UserList from './components/UserList';
 import './App.scss';
 
